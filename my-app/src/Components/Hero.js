3f71cd355fa1e08b75c33f35e8065cf5da12733a
Shahbaaz <59239856+Shahbaaz92@users.noreply.github.com>
2023-12-06 15:18:41 +0530

    App.css update

--- my-app/src/Components/Hero.js
+++ my-app/src/Components/Hero.js
@@ -2,15 +2,18 @@ import curve1 from "../assets/pattern-curved-line-1.svg";
 function Hero() {
   return (
     <section className="Hero-Container">
-      <img
-        src={curve1}
-        alt="curved line design in hero"
-        className="Hero-Curve"
-      />
-
       <div className="Hero">
         <h1>
-          <span>A</span> super solution <span>for your</span> business.
+          <span>A</span> super&nbsp;
+          <span className="Hero-Check">
+            <img
+              src={curve1}
+              alt="curved line design in hero"
+              className="Hero-Curve"
+            />
+            solution
+          </span>{" "}
+          <span>for your</span> business.
         </h1>
         <p className="Hero-about">
           Our marketing and sales automations help you scale your outreach to
